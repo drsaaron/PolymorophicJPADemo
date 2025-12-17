9d62f86759585e5db6efe4ec9c18e3eedc2e6207
AARON <scottaaron@northwesternmutual.com>
2025-12-17 10:55:43 -0600

    add some swagger doc to elements to show how to do so

--- src/main/java/com/blazartech/polymorophicjpademo/websiteuser/data/jpa/WebsiteUser.java
+++ src/main/java/com/blazartech/polymorophicjpademo/websiteuser/data/jpa/WebsiteUser.java
@@ -4,11 +4,14 @@
  */
 package com.blazartech.polymorophicjpademo.websiteuser.data.jpa;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,6 +30,11 @@ public class WebsiteUser {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Schema(description = "the user's name")
     private String name;
+    
+    @Email(regexp=".*@.*\\..*", message = "Email should be valid")
+    @Schema(description = "the user's email")
     private String email;
 }
--- src/main/java/com/blazartech/polymorophicjpademo/websiteuser/data/jpa/repo/WebsiteUserRepository.java
+++ src/main/java/com/blazartech/polymorophicjpademo/websiteuser/data/jpa/repo/WebsiteUserRepository.java
@@ -5,6 +5,8 @@
 package com.blazartech.polymorophicjpademo.websiteuser.data.jpa.repo;
 
 import com.blazartech.polymorophicjpademo.websiteuser.data.jpa.WebsiteUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -15,8 +17,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * @author aar1069
  */
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
+@Tag(description = "a simple HATEOS API for users", name = "website users API")
 public interface WebsiteUserRepository extends JpaRepository<WebsiteUser, Long>, PagingAndSortingRepository<WebsiteUser, Long> {
     
+    @Operation(summary = "find users by name")
     Collection<WebsiteUser> findByName(String name);
     
 }
